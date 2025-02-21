319f4d0a3ccd89958119ea828122635c15167fbe
francinii <francini113@gmail.com>
2025-02-20 22:38:50 -0600

    init commit

---
+++ api/main.py
@@ -0,0 +1,9 @@
+from typing import List, Union
+from fastapi import FastAPI
+from services.service import Service
+
+app = FastAPI()
+
+@app.get("/get-model")
+async def get_model_result(item: float, item2: float):
+    return Service.call_model(item, item2)
---
+++ services/service.py
@@ -0,0 +1,11 @@
+
+
+class Service():
+    def __init__(self):
+        pass
+
+
+    def call_model(items: float, items2: float,  items3: float):
+        #model = joblib.load('model.pkl')
+        #return model.predict(X_test)
+        return "Hola mundo"
