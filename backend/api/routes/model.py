1d54d30be943ecc40efabc9a2b98839d66d4fc8c
francinii <francini113@gmail.com>
2025-03-09 18:27:04 -0600

    motor de inferencia

--- backend/api/routes/model.py
+++ backend/api/routes/model.py
@@ -1,7 +1,13 @@
 from fastapi import APIRouter
+from services.inference_motor_service import InferenceMotorServices
+from models.request_models import Applicant
 from services.service import Service
 
 router = APIRouter()
 @router.get("/get-model")
 async def get_model_result(item: float, item2: float):
     return Service().call_model(item, item2)
+
+@router.post("/sbc-model")
+async def get_model_sbc_result(applicant: Applicant):
+    return Service().call_motor_inference(applicant)
--- backend/services/service.py
+++ backend/services/service.py
@@ -1,11 +1,18 @@
-
+import os
+from models.request_models import Applicant
+from services.inference_motor_service import InferenceMotorServices
 
 class Service():
     def __init__(self):
         pass
 
-
     def call_model(self, items: float, items2: float):
         #model = joblib.load('model.pkl')
         #return model.predict(X_test)
         return "Hola mundo {} {} ".format(items, items2)
+    
+    def call_model_ml(self, items: float, items2: float):
+        return "Hola mundo {} {} ".format(items, items2)    
+    
+    def call_motor_inference(self, applicant: Applicant):       
+        return InferenceMotorServices('/app/data/rules.json').inference_call(applicant)
