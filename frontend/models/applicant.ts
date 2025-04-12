076409da082dc47166348a47710d1fc98a8c4a72
Jose Andres Lizano <jose.lizano@rossmon.co.cr>
2025-04-12 00:23:57 -0600

    Applicant - Temporal new ApplicantV2 interface

--- frontend/models/applicant.ts
+++ frontend/models/applicant.ts
@@ -14,3 +14,29 @@ export interface Applicant {
     delay_from_due_date: number;
     balance_mensual: number;
 }
+
+export interface ApplicantV2 {
+	cedula: string;
+	nombre: string;
+	apellido: string;
+	tipoDocumento: string;
+	fecha_nacimiento: string;
+	edad: number; // calculado
+	salario_mensual: number; // Salario neto USD, después de los rebajos
+    saldo_deuda_pendiente: number; // total pendiente USD
+    cantidad_tarjetas_credito: number;
+    paga_monto_mínimo: boolean; // (Cómo paga la tarjetas, booleano)
+	monto_inversion_mensual: number; // USD Cuanto invierte por mes
+	fecha_experiencia_crediticia: string;
+	experiencia_crediticia: number; // (En meses) Calculado de la fecha 
+	                               // inició a usar tarjetas de crédito.
+	cantidad_prestamos_activos: number;
+	delay_from_due_date: number; // Cantidad de días
+	balance_mensual: number; // USD
+	score_credito: number;
+
+	//----- Estos ya no -----
+	// deuda_total: number;
+	// meses_trabajando
+	// cuota_mensual_total: number;
+}
